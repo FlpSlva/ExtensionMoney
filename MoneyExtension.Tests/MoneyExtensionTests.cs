dede28f482f3e4277260ac7c5b55e758b56c9b91
FlpSlva <flpslvadev@gmail.com>
2023-02-01 11:03:58 -0300

    Adicionar arquivos de projeto.

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,16 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyExtension
+{
+    public static class MoneyExtension
+    {
+        public static int ToCents(this decimal amount)
+        {
+            if (amount <= 0)
+                return 0;
+
+            var text = amount
+                .ToString("N2")
+                .Replace(",", "")
+                .Replace(".", "");
+
+            if(string.IsNullOrEmpty(text))
+                return 0;
+
+            int.TryParse(text, out var result);
+                return result;
+        }
+    }
+}
